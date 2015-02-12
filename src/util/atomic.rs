8a5445386de9b3da915890c9eea6544b75de0501
Carl Lerche <me@carllerche.com>
2015-02-12 13:36:47 -0800

    Add AtomicVal

--- src/util/atomic.rs
+++ src/util/atomic.rs
@@ -16,6 +16,106 @@ pub use self::types::{
  *
  */
 
+/// An atomic box
+pub trait Atomic<T> : Send + Sync {
+
+    /// Returns a new atomic box
+    fn new(val: T) -> Self;
+
+    /// Atomically loads the value from the box with the given ordering
+    fn load(&self, order: Ordering) -> T;
+
+    /// Atomically stores the value from the box with the given ordering
+    fn store(&self, val: T, order: Ordering);
+
+    /// Atomically swaps the value in the box with the given ordering
+    fn swap(&self, val: T, order: Ordering) -> T;
+
+    /// Swaps the value in the box if and only if the existing value is equal
+    /// to `old`.
+    fn compare_and_swap(&self, old: T, new: T, order: Ordering) -> T;
+}
+
+/// A value that can be stored in an atomic box
+pub trait ToAtomicRepr : Send {
+
+    /// The representation of the value when stored in an atomic box.
+    type Repr;
+
+    /// Load the value from the raw representation
+    fn from_repr(raw: Self::Repr) -> Self;
+
+    /// Convert the value from the raw representation
+    fn to_repr(self) -> Self::Repr;
+}
+
+// TODO: Move A -> ToAtomicRepr associated type (blocked rust-lang/rust#20772)
+pub struct AtomicVal<T: ToAtomicRepr, A: Atomic<T::Repr>> {
+    atomic: A,
+}
+
+impl<T: ToAtomicRepr, A: Atomic<T::Repr>> AtomicVal<T, A> {
+    /// Returns a new atomic box
+    pub fn new(init: T) -> AtomicVal<T, A> {
+        AtomicVal { atomic: <A as Atomic<T::Repr>>::new(init.to_repr()) }
+    }
+}
+
+impl<T: ToAtomicRepr, A: Atomic<T::Repr>> Atomic<T> for AtomicVal<T, A> {
+
+    /// Returns a new atomic box
+    fn new(init: T) -> AtomicVal<T, A> {
+        AtomicVal::new(init)
+    }
+
+    /// Atomically loads the value from the box with the given ordering
+    fn load(&self, order: Ordering) -> T {
+        let repr = self.atomic.load(order);
+        ToAtomicRepr::from_repr(repr)
+    }
+
+    /// Atomically stores the value from the box with the given ordering
+    fn store(&self, val: T, order: Ordering) {
+        self.atomic.store(val.to_repr(), order);
+    }
+
+    /// Atomically swaps the value in the box with the given ordering
+    fn swap(&self, val: T, order: Ordering) -> T {
+        let repr = self.atomic.swap(val.to_repr(), order);
+        ToAtomicRepr::from_repr(repr)
+    }
+
+    /// Swaps the value in the box if and only if the existing value is equal
+    /// to `old`.
+    fn compare_and_swap(&self, old: T, new: T, order: Ordering) -> T {
+        let repr = self.atomic.compare_and_swap(old.to_repr(), new.to_repr(), order);
+        ToAtomicRepr::from_repr(repr)
+    }
+}
+
+impl Atomic<usize> for AtomicUsize {
+
+    fn new(val: usize) -> AtomicUsize {
+        AtomicUsize::new(val)
+    }
+
+    fn load(&self, order: Ordering) -> usize {
+        AtomicUsize::load(self, order)
+    }
+
+    fn store(&self, val: usize, order: Ordering) {
+        AtomicUsize::store(self, val, order)
+    }
+
+    fn swap(&self, val: usize, order: Ordering) -> usize {
+        AtomicUsize::swap(self, val, order)
+    }
+
+    fn compare_and_swap(&self, old: usize, new: usize, order: Ordering) -> usize {
+        AtomicUsize::compare_and_swap(self, old, new, order)
+    }
+}
+
 #[cfg(target_pointer_width = "64")]
 mod types {
     use std::sync::atomic::{AtomicUsize, AtomicIsize, Ordering};
