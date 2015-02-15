6cc4f4c72478a8fdfd1e1a816628d1570f3d94c8
Carl Lerche <me@carllerche.com>
2015-02-15 03:04:39 +0000

    rebuild pages at 1198c95

--- implementors/core/marker/trait.Send.js
+++ implementors/core/marker/trait.Send.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['syncbox'] = [];
+implementors['syncbox'] = ["<a class='stability Unstable' title='Unstable: will be overhauled with new lifetime rules; see RFC 458'></a>impl&lt;A: <a class='trait' href='syncbox/util/async/trait.Async.html' title='syncbox::util::async::Async'>Async</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='syncbox/util/async/struct.Receipt.html' title='syncbox::util::async::Receipt'>Receipt</a>&lt;A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
