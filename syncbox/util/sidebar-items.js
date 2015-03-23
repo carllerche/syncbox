c6446e8051d92ff174c1a0c5e34a3056bc957834
Carl Lerche <me@carllerche.com>
2015-03-23 18:20:45 +0000

    rebuild pages at f2c3ca0

--- syncbox/util/sidebar-items.js
+++ syncbox/util/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["async","Composable primitives for asynchronous computations"],["atomic",""]],"struct":[["LinkedQueue","A queue in which values are contained by a linked list."],["ThreadPool",""]],"trait":[["Queue",""],["Run",""],["SyncQueue",""]]});
+initSidebarItems({"mod":[["async","Composable primitives for asynchronous computations"],["atomic",""]],"trait":[["Queue",""],["Run",""],["SyncQueue",""]],"struct":[["ArrayQueue",""],["LinkedQueue","A queue in which values are contained by a linked list."],["ThreadPool",""]]});
