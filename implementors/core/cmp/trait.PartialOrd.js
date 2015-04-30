abc57e0b9ac11efe4b85ec4295806937e26b9e07
Carl Lerche <me@carllerche.com>
2015-04-30 21:12:00 +0000

    rebuild pages at f58d3ff

--- implementors/core/cmp/trait.PartialOrd.js
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['syncbox'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='struct' href='syncbox/struct.Delay.html' title='syncbox::Delay'>Delay</a>&lt;T&gt;",];
+implementors['syncbox'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='struct' href='syncbox/struct.Delay.html' title='syncbox::Delay'>Delay</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
