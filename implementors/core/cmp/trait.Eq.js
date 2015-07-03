576d0c1c3e755c52426d3dee48664acd9b660168
Carl Lerche <me@carllerche.com>
2015-07-03 22:31:50 +0000

    rebuild pages at 8e936dc

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['syncbox'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='syncbox/struct.Delay.html' title='syncbox::Delay'>Delay</a>&lt;T&gt;",];
+implementors['syncbox'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='syncbox/struct.Delay.html' title='syncbox::Delay'>Delay</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
