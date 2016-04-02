7dc13513119040982aaf3432e6f287f578686420
Carl Lerche <me@carllerche.com>
2016-04-02 04:36:48 +0000

    rebuild pages at ae5c0ec

--- implementors/core/ops/trait.Deref.js
+++ implementors/core/ops/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['syncbox'] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='syncbox/struct.Delay.html' title='syncbox::Delay'>Delay</a>&lt;T&gt;",];
+implementors['syncbox'] = ["impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='syncbox/struct.Delay.html' title='syncbox::Delay'>Delay</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
