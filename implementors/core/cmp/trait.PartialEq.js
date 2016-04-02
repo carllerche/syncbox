618da906e83dc89154765b3e1e9a33ec784e4bf7
Carl Lerche <me@carllerche.com>
2016-04-02 04:36:52 +0000

    rebuild pages at ae5c0ec

--- implementors/core/cmp/trait.PartialEq.js
+++ implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['syncbox'] = ["impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='syncbox/struct.Delay.html' title='syncbox::Delay'>Delay</a>&lt;T&gt;",];
+implementors['syncbox'] = ["impl&lt;T: <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='syncbox/struct.Delay.html' title='syncbox::Delay'>Delay</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
